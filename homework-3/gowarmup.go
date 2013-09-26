04b9edd25dbeb33ffda6a856f3d933c941273bc4
frodosamoa <andrewrkowalczyk@gmail.com>
2013-09-26 11:16:59 -0700

    added gowarmup.go file

---
+++ homework-3/gowarmup.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func change(cents) {
+	
+}
+
+func stripVowels(string) {
+	
+}
+
+func scramble(string) {
+	
+}
+
+func powersOfTwo(limit) {
+	
+}
+
+func powers(base, limit) {
+	
+}
+
+func interleave(array1, array2) {
+	
+}
+
+func stutter(array) {
+	
+}
