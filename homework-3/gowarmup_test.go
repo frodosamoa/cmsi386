2d307d1abb5627890b93e5f85cb155684a532bed
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-07 15:44:21 -0700

    added the skeleton for testing gowarmup.go

---
+++ homework-3/gowarmup_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing" 
+
+func testChange(t *testing.T) {
+	
+}
+
+func testStripVowels(t *testing.T) {
+	
+}
+
+func testScramble(t *testing.T) {
+	
+}
+
+func testPowersOfTwo(t *testing.T) {
+	
+}
+
+func testPowers(t *testing.T) {
+	
+}
+
+func testInterleave(t *testing.T) {
+	
+}
+
+func testStutter(t *testing.T) {
+	
+}
