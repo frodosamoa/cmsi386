7aec8da44fdf6f3bdbe4a69b34d39460e0c750cc
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-07 15:43:44 -0700

    removed unneccesary imports, denomination in change, and semicolons throughout. interleave and stutter are now complete.

--- homework-3/gowarmup.go
+++ homework-3/gowarmup.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"regexp"
 	"math/rand"
 	"strings"
 	"time"
-	//"builtin"
 )
 
 // Returns a tuple containing, respectively, the smallest number of U.S. quarters, dimes, nickels, and pennies that equal the given amount.
 
 func change(cents int) []int {
-	change, denoms, remaining := make([]int, 4), [4]int{25, 10, 5, 1}, cents
+	change, denoms, remaining := make([]int, 4), [4]int{25, 10, 5}, cents
 
 	change[0] = remaining / denoms[0]
 	remaining %= denoms[0]
@@ -38,12 +36,12 @@ func scramble(s string) string {
 
     for i := len(a); i > 0; i -= 1 {
         r = random(0, i)
-        c = a[i-1];
-        a[i-1] = a[r];
-        a[r] = c;
+        c = a[i-1]
+        a[i-1] = a[r]
+        a[r] = c
     }
 
-    return strings.Join(a, "");
+    return strings.Join(a, "")
 }
 
 // Random int generator between min and max. 
@@ -73,16 +71,16 @@ func powers(base int, limit int, f func(int) int) {
     }	
 }
 
-// Interleaves two slices.
+// Interleaves two slices of type int.
 
 func interleave(a []int, b []int) []int {
-	c := []int{}
+	l := len(a) + len(b)
+	c := make([]int, 0, l)
 
-	for i := 0; i < len(c); i++ {
+	for i := 0; i < l; i++ {
 		if i < len(a) {
-			c = append(c, 3)
+			c = append(c, a[i])
 		}
-
 		if i < len(b) {
 			c = append(c, b[i])
 		}
@@ -91,21 +89,12 @@ func interleave(a []int, b []int) []int {
 	return c
 }
 
-// Stutters a sequence, i.e. (4, 3) ⇒ (4, 4, 3, 3)
+// Stutters a slice of type int, i.e. (4, 3) ⇒ (4, 4, 3, 3)
 
 func stutter(a []int) []int {
 	return interleave(a, a)
 }
 
-func print(x int) int {
-	return x
-}
-
 func main() {
-	fmt.Println("poop")
-	a := []int{1, 2, 3}
-	b := []int{4, 5, 6, 7, 8}
-	c := interleave(a, b)
-	fmt.Println(c)
-	powersOfTwo(800, print)
+
 }
