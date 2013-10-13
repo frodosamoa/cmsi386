5a8ce3f49178cd1a16cc439de2022d498083d720
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-12 23:56:17 -0700

    added more tests, working on combining all array equality tests into one, added comments

--- homework-3/gowarmup/gowarmup_test.go
+++ homework-3/gowarmup/gowarmup_test.go
@@ -2,7 +2,10 @@ package gowarmup
 
 import (
 	"testing"
+	"fmt"
 )
+
+// Returns true if two int arrays/slices are equal.
 
 func testEq(a, b []int) bool {
     if len(a) != len(b) {
@@ -18,9 +21,13 @@ func testEq(a, b []int) bool {
     return true
 }
 
+// Test are stored in struct sfor table driven testing. Each one is formatted
+// as so. Actual output, then the expected output, denoted by those respecive
+// words.
+
 var changeTests = []struct {
-        in  []int
-        out []int
+        actual  	[]int
+        expected 	[]int
 }{
         {change(97), []int{3, 2, 0, 2}},
         {change(8), []int{0, 0, 1, 3}},
@@ -38,35 +45,93 @@ var changeTests = []struct {
         // {change(-30000), []int{0, 0, 0, 0}},
 }
 
+var stripVowelsTests = []struct {
+		actual 		string
+		expected 	string
+}{
+	{stripVowels("Hello, world"), "Hll, wrld"},
+	{stripVowels("I like turtles"), " lk trtls"},
+	{stripVowels("aeiou"), ""},
+	{stripVowels("bcdfghjklmnpqrstvwxyz"), "bcdfghjklmnpqrstvwxyz"},
+	{stripVowels("abecidoeuf"), "bcdf"},
+	{stripVowels("Happiness depends upon ourselves"), "Hppnss dpnds pn rslvs"},
+	{stripVowels("Chrononhotonthologos"), "Chrnnhtnthlgs"},
+	{stripVowels("HONORIFICABILITUDINITATIBUS"), "HNRFCBLTDNTTBS"},
+	{stripVowels("Philip M. Dorin, John David N. Dionisio, Ray Toal, Masao Kitamura"), "Phlp M. Drn, Jhn Dvd N. Dns, Ry Tl, Ms Ktmr"},
+	{stripVowels("`~!@#$^&*()_+-=[]{};./>?"), "`~!@#$^&*()_+-=[]{};./>?"},
+}
 
-func TestChange(t *testing.T) {
-    for i, change := range changeTests {
-		if (!testEq(change.in, change.out)) {
-			t.Errorf("Test %d: expected %v got %v", i, change.in, change.out)
+var interleaveTests = []struct {
+		actual 		[]int
+		expected 	[]int
+}{
+	{interleave([]int{},[]int{}), []int{}},
+	{interleave([]int{0, 2, 4, 6, 8}, []int{1, 3, 5, 7, 9}), []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+	// {interleave(), },
+}
+
+var stutterTests = []struct {
+		actual 		[]int
+		expected 	[]int
+}{
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+	// {stutter(), },
+}
+
+func testIntArrayStructs(tests []struct{}, t *testing.T) {
+	fmt.Println(tests)
+    for i, test := range tests {
+		if (!testEq(test.actual, test.expected)) {
+			t.Errorf("Test %d: expected %v got %v", i, test.actual, test.expected)
 		}
 	}
 }
 
-func testStripVowels(t *testing.T) {
+// TESTS
+
+func TestChange(changeTests []struct{}, t *testing.T) {
+	testIntArrayStructs(changeTests, t)
+}
+
+func TestStripVowels(t *testing.T) {
+	for i, str := range stripVowelsTests {
+		if (str.actual != str.expected) {
+			t.Errorf("Test %d: expected %v got %v", i, str.actual, str.expected)
+		}
+	}
+}
+
+func TestScramble(t *testing.T) {
 	
 }
 
-func testScramble(t *testing.T) {
+func TestPowersOfTwo(t *testing.T) {
 	
 }
 
-func testPowersOfTwo(t *testing.T) {
+func TestPowers(t *testing.T) {
 	
 }
 
-func testPowers(t *testing.T) {
-	
+func TestInterleave(interleaveTests []struct{}, t *testing.T) {
+	testIntArrayStructs(interleaveTests, t)
 }
 
-func testInterleave(t *testing.T) {
-	
-}
-
-func testStutter(t *testing.T) {
-	
+func TestStutter(stutterTests []struct{}, t *testing.T) {
+	testIntArrayStructs(stutterTests, t)
 }
