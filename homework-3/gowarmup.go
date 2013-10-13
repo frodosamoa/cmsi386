b8042588805a523a23e0e4bb01105035957a9052
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-12 20:07:06 -0700

    editing the gowamup files, added one test and renamed the package

--- homework-3/gowarmup.go
+++ homework-3/gowarmup.go
@@ -1,4 +1,4 @@
-package main
+package gowarmup
 
 import (
 	"regexp"
--- homework-3/gowarmup_test.go
+++ homework-3/gowarmup_test.go
@@ -1,9 +1,31 @@
-package main
+package gowarmup
 
-import "testing" 
+import (
+	"testing" 
+	"fmt"
+)
+
+func testEq(a, b []int) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i := range a {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+
+    return true
+}
 
 func testChange(t *testing.T) {
-	
+	cents := change(97)
+	fmt.Println(cents)
+	change := []int{3, 2, 0, 2}
+	if (testEq(cents, change)) {
+		t.Errorf("Expected [3, 2, 0, 2] got ", change)
+	}
 }
 
 func testStripVowels(t *testing.T) {
