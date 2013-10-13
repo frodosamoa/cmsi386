f97e5de539129c8c0ec4909eac6b70b9bdd0a443
frodosamoa <andrewrkowalczyk@gmail.com>
2013-10-12 22:57:44 -0700

    added ftests for change(), todo, negative amount of cents, makes no sense

--- homework-3/gowarmup/gowarmup_test.go
+++ homework-3/gowarmup/gowarmup_test.go
@@ -2,7 +2,6 @@ package gowarmup
 
 import (
 	"testing"
-	"fmt" 
 )
 
 func testEq(a, b []int) bool {
@@ -24,14 +23,26 @@ var changeTests = []struct {
         out []int
 }{
         {change(97), []int{3, 2, 0, 2}},
-        {change(8), []int{0, 0, 1, 3)}
+        {change(8), []int{0, 0, 1, 3}},
+        {change(1412873456), []int{56514938, 0, 1, 1}},
+        {change(41), []int{1, 1, 1, 1}},
+        {change(890), []int{35, 1, 1, 0}},
+        {change(4), []int{0, 0, 0, 4}},
+        {change(123456789), []int{4938271, 1, 0, 4}},
+        {change(250), []int{10, 0, 0, 0}},
+        {change(519), []int{20, 1, 1, 4}},
+        {change(1111), []int{44, 1, 0, 1}},
+
+        // TODO: Negative change
+        // {change(-2), []int{0, 0, 0, 0}},
+        // {change(-30000), []int{0, 0, 0, 0}},
 }
 
 
 func TestChange(t *testing.T) {
     for i, change := range changeTests {
-		if (testEq(change.in, change.out)) {
-			t.Errorf("Expected %s got %s", change.in, change.out)
+		if (!testEq(change.in, change.out)) {
+			t.Errorf("Test %d: expected %v got %v", i, change.in, change.out)
 		}
 	}
 }
